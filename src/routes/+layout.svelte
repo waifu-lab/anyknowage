d3306bbe5fca73707ff6b302d4bf5214053f373c
phillychi3 <phillychi3@gmail.com>
2024-04-30 15:53:33 +0800

    feat: add shadcn-svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,14 @@
 <script>
+	import '../app.pcss';
 	import Header from './Header.svelte';
 	import './styles.css';
 </script>
 
 <div class="app">
-	<Header />
+	<Header></Header>
 
 	<main>
-		<slot />
+		<slot></slot>
 	</main>
 
 	<footer>
