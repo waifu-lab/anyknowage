243c8a3189b5dcc89a40d3e034883fc540455915
phillychi3 <phillychi3@gmail.com>
2024-05-27 02:35:23 +0800

    feat: add mote type (wait

---
+++ server/modules/knowladge/image.py
@@ -0,0 +1,17 @@
+from modules.knowladge.basic import basic_file_parser
+from models.file import File
+from tempfile import NamedTemporaryFile
+from pathlib import Path
+import os
+
+
+def image_file_parser(file: File):
+    with NamedTemporaryFile(delete=False) as temp:
+        temp.write(file.file)
+        temp_path = Path(temp.name)
+    try:
+        # TODO:
+        text = ...
+    finally:
+        os.remove(temp_path)
+    return basic_file_parser([file.get_Document(text)])
--- server/modules/knowladge/url.py
+++ server/modules/knowladge/url.py
@@ -1,6 +1,7 @@
-from .basic import basic_file_parser
+from modules.knowladge.basic import basic_file_parser
 from models.file import File
 
 
 def parse_url(file: File):
+    # TODO:
     basic_file_parser()
