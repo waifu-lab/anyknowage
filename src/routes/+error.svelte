d125fc45814c7d22d09d69f2d04aed5e138a8d3b
phillychi3 <phillychi3@gmail.com>
2024-05-30 15:12:35 +0800

    feat: add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { page } from '$app/stores'
+</script>
+
+<div>
+	{#if $page?.error}
+		<div class="mt-4 border-y-2 p-4">
+			{#if $page?.status}
+				<img src="https://http.cat/{$page?.status}" alt="HTTP status code" />
+				<p>Page status: {$page?.status}</p>
+			{/if}
+			{#if $page?.error?.message}
+				<p>Error message: {$page?.error?.message}</p>
+			{/if}
+		</div>
+	{/if}
+</div>
