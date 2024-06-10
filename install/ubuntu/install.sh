9f768dcb4c020b30d5c39ccf03cea9dc1787496f
phillychi3 <phillychi3@gmail.com>
2024-06-10 22:51:38 +0800

    feat: add ubuntu install script

---
+++ install/ubuntu/install.sh
@@ -0,0 +1,14 @@
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+git clone https://github.com/waifu-lab/anyknowage
+cd anyknowage
+sudo docker compose -f docker-compose.yml up -d
