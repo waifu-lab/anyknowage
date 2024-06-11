b4cad924cca9763f6cec81e78643e7b1c9e6d250
phillychi3 <phillychi3@gmail.com>
2024-06-11 11:26:17 +0800

    fix: 修復初始化無法顯示問題

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,5 +1,27 @@
 import { setup_socketio } from '$lib/socketio'
 import toast from 'svelte-french-toast'
+if (localStorage.getItem('default') === null) {
+	localStorage.setItem(
+		'default',
+		JSON.stringify({
+			server_url: 'http://localhost:8000',
+			username: 'user',
+			avatar: 'https://cdn.discordapp.com/avatars/762484891945664542/a3d0e4d30b78ce30a2ed22b51bf80df4.png?size=1024',
+			botavatar:
+				'https://cdn.discordapp.com/avatars/762484891945664542/a3d0e4d30b78ce30a2ed22b51bf80df4.png?size=1024'
+		})
+	)
+}
+if (localStorage.getItem('chat') === null) {
+	localStorage.setItem(
+		'chat',
+		JSON.stringify({
+			model: 'gpt-4o',
+			maxtoken: 2000,
+			openaikey: ''
+		})
+	)
+}
 const io = setup_socketio()
 io.on('connect', () => {
 	console.log('connected')
